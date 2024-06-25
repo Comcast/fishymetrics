0cce13950e631c975aaf285ff33f252bc41193a5
Jennifer Kaiser <65861760+jenniferKaiser21@users.noreply.github.com>
2024-06-24 21:02:11 -0400

    exporter.go 6/24 changes

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"crypto/tls"
 	"errors"
+	"fmt"
 	"net"
 	"net/http"
 	"net/url"
@@ -311,21 +312,35 @@ func NewExporter(ctx context.Context, target, uri, profile, model string, exclud
 		zap.Strings("physical_drive_endpoints", driveEndpointsResp.physicalDriveURLs),
 		zap.Any("trace_id", ctx.Value("traceID")))
 
+	// // Using initial /UpdateService/FirmwareInventory endpoint, grab all of the Firmware Inventory URLs
+	// firmwareInventoryEndpoints, err := getMemberUrls(exp.url+uri+"/UpdateService/FirmwareInventory/", target, retryClient)
+	// if err != nil {
+	// 	log.Error("error when getting FirmwareInventory url", zap.Error(err), zap.Any("trace_id", ctx.Value("traceID")))
+	// 	firmwareInventoryEndpoints = []string{}
+	// }
+
 	// Using initial /UpdateService/FirmwareInventory endpoint, grab all of the Firmware Inventory URLs
+	// Call /redfish/v1/Managers/XXXX/UpdateService/FirmwareInventory/ for firmware inventory
 	firmwareInventoryEndpoints, err := getMemberUrls(exp.url+uri+"/UpdateService/FirmwareInventory/", target, retryClient)
 	if err != nil {
 		// Try the iLo 4 firmware inventory endpoint
-		firmwareInventoryResp, err := getFirmwareComponents(exp.url+uri+"/Systems/1/FirmwareInventory/", target, retryClient)
-		if err != nil {
-			log.Error("error when getting FirmwareInventory url", zap.Error(err), zap.Any("trace_id", ctx.Value("traceID")))
-			return nil, err
+		// Use the collected sysEndpoints.systems to build url(s)
+		if len(sysEndpoints.systems) > 0 {
+			// call /redfish/v1/Systems/XXXX/FirmwareInventory/
+			for _, system := range sysEndpoints.systems {
+				firmwareInventoryEndpoints = append(firmwareInventoryEndpoints, system+"FirmwareInventory/")
+			}
+			// Ensure we have at least one firmware inventory endpoint
+			if len(firmwareInventoryEndpoints) == 0 {
+				log.Error("error when getting FirmwareInventory url", zap.Error(err), zap.Any("trace_id", ctx.Value("traceID")))
+				return nil, err
+			}
 		}
-		firmwareInventory := firmwareInventoryResp.Current
+	}
 
-		// Loop through firmwareInventory and set metrics
-		for _, Component := range firmwareInventory {
-			// TODO FINISH STREAMING METRICS HERE
-		}
+	// Loop through logicalDriveURLs, physicalDriveURLs, and nvmeDriveURLs and append each URL to the tasks pool
+	for _, url := range driveEndpointsResp.logicalDriveURLs {
+		tasks = append(tasks, pool.NewTask(common.Fetch(exp.url+url, target, profile, retryClient), exp.url+url, handle(&exp, LOGICALDRIVE)))
 	}
 
 	for _, url := range driveEndpointsResp.physicalDriveURLs {
@@ -360,6 +375,9 @@ func NewExporter(ctx context.Context, target, uri, profile, model string, exclud
 
 	// Firmware Inventory
 	for _, url := range firmwareInventoryEndpoints {
+		// DEBUG PRINT
+		fmt.Printf("task url to be appended: %s\n", exp.url+url)
+		// END DEBUG PRINT
 		tasks = append(tasks, pool.NewTask(common.Fetch(exp.url+url, target, profile, retryClient), exp.url+url, handle(&exp, FIRMWAREINVENTORY)))
 	}
 
